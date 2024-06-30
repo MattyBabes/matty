85e40f3d5e07d4ab5746cf206130622d31b8ace8
MattyBabes <167733187+MattyBabes@users.noreply.github.com>
2024-06-30 02:14:13 -0700

    Add files via upload

---
+++ MatthewRom.github.io-main/main.js
@@ -0,0 +1,11 @@
+
+var typed = new Typed(".deftext", {
+    strings: ["a Dancer.", "a Poet.", "umbrageous.", "a Guitar Player.", "a Public Speaker."],
+    typeSpeed:40,
+    backSpeed:40,
+    backDelay:1000,
+    loop:true});
+
+
+
+    
